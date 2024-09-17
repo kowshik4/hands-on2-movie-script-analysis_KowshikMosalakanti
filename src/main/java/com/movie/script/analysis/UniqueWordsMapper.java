4b5fd7721df55e8a1053ce1d3045629f86cca2ce
Kowshik sai Mosalakanti <kowshik.sai98@gmail.com>
2024-09-17 20:04:09 +0000

    Completed Movie Script Analysis Assignment

--- src/main/java/com/movie/script/analysis/UniqueWordsMapper.java
+++ src/main/java/com/movie/script/analysis/UniqueWordsMapper.java
@@ -14,6 +14,21 @@ public class UniqueWordsMapper extends Mapper<Object, Text, Text, Text> {
 
     @Override
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-
+        StringTokenizer itr = new StringTokenizer(value.toString());
+        String characterName = "";
+        HashSet<String> uniqueWords = new HashSet<>();
+        while (itr.hasMoreTokens()) {
+            String token = itr.nextToken();
+            if (token.startsWith("<b>") && token.endsWith("</b>")) {
+                characterName = token.substring(3, token.length() - 4);
+            } else {
+                if (!uniqueWords.contains(token)) {
+                    uniqueWords.add(token);
+                    character.set(characterName);
+                    word.set(token);
+                    context.write(character, word);
+                }
+            }
+        }
     }
 }
--- src/main/java/com/movie/script/analysis/UniqueWordsReducer.java
+++ src/main/java/com/movie/script/analysis/UniqueWordsReducer.java
@@ -10,6 +10,14 @@ public class UniqueWordsReducer extends Reducer<Text, Text, Text, Text> {
 
     @Override
     public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-
+        HashSet<String> uniqueWords = new HashSet<>();
+        for (Text value : values) {
+            uniqueWords.add(value.toString());
+        }
+        StringBuilder uniqueWordsList = new StringBuilder();
+        for (String word : uniqueWords) {
+            uniqueWordsList.append(word).append(", ");
+        }
+        context.write(key, new Text(uniqueWordsList.toString()));
     }
 }
